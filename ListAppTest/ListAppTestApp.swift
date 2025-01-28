474ff4554808c52d7be6f65487a6fa0f64c5cef4
ffdossa <127089701+ffdossa@users.noreply.github.com>
2025-01-28 15:36:02 +0200

    Initial Commit

---
+++ ListAppTest/ListAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppTestApp.swift
+//  ListAppTest
+//
+//  Created by Andrii Marchuk on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ListAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
